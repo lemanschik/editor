be1a402ded84a5fe0855bbe162ef94c2f789751a
Frank Lemanschik <frank@lemanschik.com>
2022-10-22 14:49:17 +0200

    Create context.js
    Exports a string as context
    
    Signed-off-by: Frank Lemanschik <frank@lemanschik.com>

---
+++ modules/context.js
@@ -0,0 +1,10 @@
+/**
+ * A Editor Context that is use able via import('./modules/context.js')
+ * You should not transpil that dynamic import as this enables dynamic
+ * references cross context via './modules/context.js?id=ContextID'
+ * note: import puts a reference into the Module Cache indexed by Url
+ */
+export const context = ``;
+
+// tip: create a handle (reference to the context) register event listners on it.
+// export them as reuseable module
